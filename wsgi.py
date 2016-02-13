fcb7359e1108ab8baa8b9c75ee774c10235dd0a3
TI_Eugene <ti.eugene@gmail.com>
2016-02-13 12:40:56 +0300

    apache cpbl

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+import os, sys, locale
+
+locale.setlocale(locale.LC_TIME,'ru_RU.utf8')
+sys.path.append('/usr/share/kupa')
 
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
